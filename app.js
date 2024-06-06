1c19504271a825d626884fe36870987c8c197cf0
marcovittoriozacca <zaccamarco01@gmail.com>
2024-06-06 16:47:04 +0200

    feat: BONUS: Error 404 page not found handler

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const posts = require('./routers/posts.js');
+const notFoundHandler = require('./middlewares/notFoundHandler.js');
 const app = express();
 
 const { PORT } = process.env;
@@ -9,6 +10,8 @@ const { HOST } = process.env;
 app.use(express.json());
 
 app.use("/posts", posts);
+
+app.use(notFoundHandler);
 
 app.listen(PORT, HOST, () => {
     console.log(`http://${HOST}:${PORT}`);
